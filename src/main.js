3f0f9a71180442b6e025d1597db42c82400219ef
lemoonchild <ncastrom21@gmail.com>
2024-04-25 17:44:18 -0600

    feat: added main.js file for API

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'bodyparser'
+
+const app = express()
+
+app.use(express.json)
+
+app.use(bodyParser.json)
+
+app.use(cors())
+
+const port = 5007
+
+app.get('/', async (req, res) => {
+  res.send('Hello world from API! owo')
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at http://127.0.0.1:${port}`)
+})
