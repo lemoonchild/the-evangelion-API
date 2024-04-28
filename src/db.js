c8208cee2bae3f30ab18e0f03f53ecbc7f501b51
lemoonchild <ncastrom21@gmail.com>
2024-04-27 20:27:55 -0600

    feat: added the getUserID async function

--- src/db.js
+++ src/db.js
@@ -1,13 +1,19 @@
 import conn from './conn.js'
 
 export async function registerUser(username, password_md5, email) {
-  const sql = `INSERT INTO users (username, password, email) VALUES ($1,MD5($2),$3)`
+  const sql = `INSERT INTO users (username, password_md5, email) VALUES ($1,MD5($2),$3)`
   await conn.query(sql, [username, password_md5, email])
   return true
 }
 
 export async function loginUser(username, password_md5) {
-  const sql = `SELECT * FROM users WHERE username = $1 AND password = MD5($2)`
+  const sql = `SELECT * FROM users WHERE username = $1 AND password_md5 = MD5($2)`
   const result = await conn.query(sql, [username, password_md5])
   return result.rows[0]
+}
+
+export async function getUserById(id) {
+  const sql = `SELECT * FROM users WHERE id = $1`
+  const result = await conn.query(sql, [id])
+  return result.rows[0].length > 0 ? result.rows : 'No user found.'
 }
